854be23cc98a2df81839984a76c56eece32127f5
kamilek7 <85680654+Kamilek7@users.noreply.github.com>
2025-12-20 16:23:22 +0100

    szkielet

--- main.js
+++ main.js
@@ -2,7 +2,19 @@ function sonntagChecken(date)
 {
 
 }
-let heutigeDate = new Date();
+
+function niedzielaDzisiaj()
+{
+
+}
+function niedzielaWTymTygodniu()
+{
+
+}
+function niedzielaDaleko()
+{
+    
+}
 let nahesteSonntag;
 let sonntagen = ["2025.4.13", "2025.4.27", "2025.6.29", "2025.8.31", "2025.12.7", "2025.12.14", "2025.12.21", "2026.1.26", "2026.4.13"]
 for (let i = 0; i < sonntagen.length; i++) 
@@ -11,15 +23,27 @@ for (let i = 0; i < sonntagen.length; i++)
 }
 function zumStart()
 {
+    let niedzielaNajblizsza=-1;
+    let niedzielaNajblizszaDate
     sonntagen.forEach((tag) => {
-        let sonntagElement = document.getElementById("niedziela-big");
-        let sonntagZusammenfassung = document.getElementById("niedziela-smol");
-        
-        if (sonntagChecken(tag)) 
+        let dni = sonntagChecken(tag);
+        if (dni<=niedzielaNajblizsza || niedzielaNajblizsza==-1)
         {
-            sonntagElement.innerHTML="Tak";
-
+            niedzielaNajblizsza=dni;
+            niedzielaNajblizszaDate = tag;
         }
-
     })
+    
+    if (niedzielaNajblizsza==0)
+    {
+        // Dzisiaj jest niedziela
+    }
+    else if (niedzielaNajblizsza<7)
+    {
+        // W tym tygodniu jest niedziela
+    }
+    else
+    {
+        // W tym tygodniu nie ma niedzieli
+    }
 }
